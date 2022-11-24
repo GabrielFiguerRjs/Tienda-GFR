419b2e30c4a874caec38f5f2b368ef1dc385efd2
GabrielFiguerRjs <114114561+GabrielFiguerRjs@users.noreply.github.com>
2022-11-24 19:01:18 -0300

    Add files via upload
    Carga inicial, faltan cosas

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+//import navbarTienda from './Components/navbarTienda';
+import BarraNav from './Components/BarraNav';
+function App() {
+  return ( 
+    <div > 
+     <BarraNav></BarraNav>
+         
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ Components/BarraNav.jsx
@@ -0,0 +1,25 @@
+
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const BarraNav = () => {
+   let CompanyName = 'Antigüedades Casilda';
+    return (
+        <div  style={{ backgroundColor: ''}}>
+        <Navbar  >
+          <Container >
+            <Navbar.Brand  href="#home">{CompanyName} </Navbar.Brand>
+                       <Navbar.Brand href="#AboutUs"> Nosotros </Navbar.Brand>
+            <Navbar.Brand href="#Gallery"> Galeria </Navbar.Brand>
+            <Navbar.Brand>
+                
+            </Navbar.Brand>
+            </Container>
+          </Navbar>
+        </div>
+        
+      );
+}
+export default BarraNav;
