c0b450b1e626fb41846b37b7001411e60a9aaeda
GabrielFiguerRjs <114114561+GabrielFiguerRjs@users.noreply.github.com>
2022-11-24 21:37:15 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './App.css'
 //import navbarTienda from './Components/navbarTienda';
 import BarraNav from './Components/BarraNav';
+import ItemListContainer from './Components/ItemListContainer';
 function App() {
   return ( 
-    <div > 
+    <div className='App'  > 
      <BarraNav></BarraNav>
          
+
+       <ItemListContainer className = '.App-header' greeting = {"Hola, Bienvenid@ a nuestro sitio"}  ></ItemListContainer>
     </div>
     
   );
